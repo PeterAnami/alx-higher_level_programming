3754f4e69f71a6c65b5ad7ab8771d5e96c4715b0
peteranami <andakepe@ueab.ac.ke>
2024-01-27 09:51:47 -0800

    networking basics

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments that lists 10 commits (from the most
+recent to oldest) of the repository and user passed in as argument using the
+Github API"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
+                                                              sys.argv[1])
+    req = requests.get(url)
+
+    try:
+        for i in range(10):
+            print("{}: {}".format(req.json()[i].get('sha'),
+                                  req.json()[i].get('commit')
+                                  .get('author').get('name')))
+    except IndexError:
+        pass
