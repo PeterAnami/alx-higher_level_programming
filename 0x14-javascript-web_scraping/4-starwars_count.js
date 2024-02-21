bb3fac6d96fcb34c7e5cd4dd366dd1a63d9e04c5
peteranami <andakepe@ueab.ac.ke>
2024-02-20 23:12:57 -0800

    js webscraping

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+
+    let count = 0;
+
+    for (let i = 0; i < films.length; i++) {
+      const characters = films[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].includes('18')) {
+          count++;
+        }
+      }
+    }
+
+    console.log(count);
+  }
+});
